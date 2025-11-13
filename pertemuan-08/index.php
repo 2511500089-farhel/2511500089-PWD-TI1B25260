eab05b363751c5855e75032beca1d004a4d7e4df
chika zayin salsabilla <2511500044@mahasiswa.atmaluhur.ac.id>
2025-11-13 12:46:27 +0700

    menambahkan nama saya dan nim saya dan tanggal lahir di proses.php

---
+++ pertemuan-08/index.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+
+$sesnama = "";
+if (isset($_SESSION["sesnama"])):
+  $sesnama = $_SESSION["sesnama"];
+endif;
+
+$sesemail = "";
+if (isset($_SESSION["sesemail"])):
+  $sesemail = $_SESSION["sesemail"];
+endif;
+
+$sespesan = "";
+if (isset($_SESSION["sespesan"])):
+  $sespesan = $_SESSION["sespesan"];
+endif;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Judul Halaman</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <h1>Ini Header</h1>
+    <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigation">
+      &#9776;
+    </button>
+    <nav>
+      <ul>
+        <li><a href="#home">Beranda</a></li>
+        <li><a href="#about">Tentang</a></li>
+        <li><a href="#contact">Kontak</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section id="home">
+      <h2>Selamat Datang</h2>
+      <?php
+      echo "halo dunia!<br>";
+      echo "nama saya hadi";
+      ?>
+      <p>Ini contoh paragraf HTML.</p>
+    </section>
+
+    <section id="Entrydata"
+        <h2>Entry data Mahasiswa</h2>
+
+        <label for="txtNama"><span>Nama:</span>
+          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
+        </label>
+
+        <label for="txtEmail"><span>Email:</span>
+          <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email" required autocomplete="email">
+        </label>
+
+        <label for="txtPesan"><span>Pesan Anda:</span>
+          <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
+          <small id="charCount">0/200 karakter</small>
+        </label>
+
+    
+
+    <section id="about">
+      <?php
+      $nim = 2511500089;
+      $Nama = 'FARHEL ARIFQI';
+      $tempat_tanggal_lahir = "TOBOALI, 7 MEI 2007"
+      $pekerjaan ="MAHASISWA ATMA LUHUR"
+      $orangtua ="NAMA BAPAK NINO PRIMADONA DAN IBU ARIANTI"
+      $pasangan = "MUDAH MUDAHAN AJA"
+      $hobi = "FUTSAL"
+      $nama_adik = "FAKHIRA AQILA"
+      ?>
+      <h2>Tentang Saya</h2>
+      <p><strong>NIM:</strong>
+        <?php
+        echo $NIM;"2511500089"
+        ?>
+      </p>
+      <p><strong>Nama Lengkap:</strong>
+        <?php
+        echo $Nama;"FARHEL ARIFQI"
+        ?> &#128526;
+      </p>
+      <p><strong>Tempat Lahir:</strong> TOBOALI</p>
+      <p><strong>Tanggal Lahir:</strong> 7 MEI 2007</p>
+      <p><strong>Hobi:</strong> FUTSAL</p>
+      <p><strong>Pasangan:</strong> BELUM ADA</p>
+      <p><strong>Pekerjaan:</strong> MAHASISWA ISB ATMA LUHUR</p>
+      <p><strong>Nama Orang Tua:</strong> BAPAK NINO PRIMADONA IBU ARIANTI</p>
+      <p><strong>Nama Adik:</strong> FAKHIRA AQILA</p>
+    </section>
+
+    <section id="contact">
+      <h2>Kontak Kami</h2>
+      <form action="proses.php" method="POST">
+
+        <label for="txtNama"><span>Nama:</span>
+          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
+        </label>
+
+        <label for="txtEmail"><span>Email:</span>
+          <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email" required autocomplete="email">
+        </label>
+
+        <label for="txtPesan"><span>Pesan Anda:</span>
+          <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
+          <small id="charCount">0/200 karakter</small>
+        </label>
+
+
+        <button type="submit">Kirim</button>
+        <button type="reset">Batal</button>
+      </form>
+
+      <?php if (!empty($sesnama)): ?>
+        <br><hr>
+        <h2>Yang menghubungi kami</h2>
+        <p><strong>Nama :</strong> <?php echo $sesnama ?></p>
+        <p><strong>Email :</strong> <?php echo $sesemail ?></p>
+        <p><strong>Pesan :</strong> <?php echo $sespesan ?></p>
+      <?php endif; ?>
+
+
+
+    </section>
+  </main>
+
+  <footer>
+    <p>&copy; 2025 FARHEL ARIFQI [2511500089]</p>
+  </footer>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
